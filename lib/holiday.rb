046047032c911ce35d93a438c11b0ea67bccb384
Max Sun <max.sun@yale.edu>
2019-05-17 16:07:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, hash|
+    puts "#{season.to_s}:"
+    
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
